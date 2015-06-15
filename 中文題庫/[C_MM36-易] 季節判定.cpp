d01ded373816723c7670d2b14e82de0e8c48b015
noname0930 <noname19930930@gmail.com>
2015-06-15 09:47:04 +0800

    Solved

---
+++ 中文題庫/[C_MM36-易] 季節判定.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        if(n > 2 && n < 6)
+            cout << "Spring";
+        else if(n > 5 && n < 9)
+            cout << "Summer";
+        else if(n > 8 && n < 12)
+            cout << "Autumn";
+        else
+            cout << "Winter";
+        cout << endl;
+    }
+    return 0;
+}
