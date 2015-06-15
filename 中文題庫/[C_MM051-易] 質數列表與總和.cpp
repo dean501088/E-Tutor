e62113abfda2212a2efec937b7e184f039e6c0b7
noname0930 <noname19930930@gmail.com>
2015-06-15 09:46:56 +0800

    Solved

--- 中文題庫/[C_MM051-易] 質數列表與總和.cpp
+++ 中文題庫/[C_MM051-易] 質數列表與總和.cpp
@@ -1,47 +1,51 @@
-#include<iostream>  
-using namespace std;  
-bool f(int x);  
-int main()  
-{  
-    int n;  
-    while(cin >> n)  
-    {  
-        int k=1, x[500], T=0, m=0;  
-        for(int i=0;i<n;i++)  
-        {  
-            for(int j=k+1;j<500;j++, k++)  
-            {                 
-                if(f(j))  
-                {  
-                    k++;  
-                    x[m]=j;  
-                    m++;  
-                    break;  
-                }  
-            }  
-        }  
-        for(int i=0;i<m;i++)  
-        {  
-            cout << x[i] << ",";  
-            T+=x[i];  
-        }  
-        cout << endl << T << endl;  
-    }  
-    return 0;  
-}  
-bool f(int x)  
-{  
-    int y=1;  
-    if(x!=2)  
-    {  
-        for(int i=2;i<sqrt((double)x)+1;i++)  
-        {  
-            if(x%i==0)  
-            {  
-                y=0;  
-                break;  
-            }  
-        }  
-    }  
-    return y;  
-}  
+#include<iostream>
+#include<vector>
+
+#define MAX 10001
+
+using namespace std;
+
+void initialize(bool prime[])
+{
+	//initialize boolean
+	prime[0] = false;
+	prime[1] = true;
+	for (int i = 2; i < MAX; i++)
+		prime[i] = true;
+
+	for (int i = 2; i < MAX; i++)
+	{
+		if (prime[i])
+		{
+			for (int j = i * 2; j < MAX; j += i)
+			{
+				prime[j] = false;
+			}
+		}
+	}
+}
+
+int main()
+{
+	bool temp[MAX];
+	initialize(temp);
+ 
+	vector<int> prime;
+	for (int i = 2; i < MAX; i++)
+		if (temp[i])
+			prime.push_back(i);
+
+	int n;
+	while (cin >> n)
+	{
+		int sum = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			cout << prime[i] << ",";
+			sum += prime[i];
+		}
+		cout << "\n" << sum << endl;
+	}
+	return 0;
+}
