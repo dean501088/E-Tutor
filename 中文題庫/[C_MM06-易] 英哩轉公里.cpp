9a95f395235a70e5f5a2ec2f751fc08a0a36dc05
noname <noname19930930@gmail.com>
2015-07-03 22:11:16 +0800

    Solved

--- 中文題庫/[C_MM06-易] 英哩轉公里.cpp
+++ 中文題庫/[C_MM06-易] 英哩轉公里.cpp
@@ -1,10 +1,14 @@
 #include<iostream>  
-#include<iomanip>  
-using namespace std;  
-int main()  
-{  
-    double x;  
-    while(cin >> x)  
-        cout << fixed << setprecision(1) << 1.6*x << endl;  
-    return 0;  
-}  
+
+using namespace std;
+
+int main()
+{
+	double x;
+	while (cin >> x)
+	{
+		printf("%.1f\n", 1.6 * x);
+		
+	}
+	return 0;
+}
