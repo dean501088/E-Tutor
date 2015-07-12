ef65c6db6677da3b89adc0abc00902ed3b5747ca
noname <noname19930930@gmail.com>
2015-07-12 13:42:52 +0800

    Solved

--- 中文題庫/[C_MM10-易] 攝氏溫度轉華式溫度.cpp
+++ 中文題庫/[C_MM10-易] 攝氏溫度轉華式溫度.cpp
@@ -1,10 +1,13 @@
 #include<iostream>  
-#include<iomanip>  
-using namespace std;  
-int main()  
-{  
-    double x;  
-    while(cin >> x)  
-        cout << fixed << setprecision(1) << (x*9/5)+32 << endl;  
-    return 0;  
-}  
+ 
+using namespace std;
+
+int main()
+{
+	double x;
+	while (cin >> x)
+	{
+		printf("%.1f\n", (x * 9 / 5) + 32);
+	}
+	return 0;
+}
