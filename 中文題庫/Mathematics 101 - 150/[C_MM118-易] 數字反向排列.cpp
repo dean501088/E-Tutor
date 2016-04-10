c42093f26e53f5e72c8a7833979e4c8b8b46782e
noname <noname19930930@gmail.com>
2016-04-11 00:31:58 +0800

    AC

---
+++ 中文題庫/Mathematics 101 - 150/[C_MM118-易] 數字反向排列.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string s;
+    while (cin >> s)
+    {
+        for (int i = 0; i < s.length(); i++)
+            cout << s[s.length() - 1 - i];
+        cout << endl;
+    }
+    return 0;
+}
