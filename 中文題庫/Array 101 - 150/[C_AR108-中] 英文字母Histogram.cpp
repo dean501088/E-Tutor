a6d8ddd6aa534e7a9066977ef2ff6918fc84d73d
noname <noname19930930@gmail.com>
2016-04-26 05:41:24 +0800

    AC

---
+++ 中文題庫/Array 101 - 150/[C_AR108-中] 英文字母Histogram.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string s;
+    while(cin >> s)
+    {
+        int count[26] = { 0 };
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] >= 'a' && s[i] <= 'z')
+                count[s[i] - 'a']++;
+            if(s[i] >= 'A' && s[i] <= 'Z')
+                count[s[i] - 'A']++;
+        }   
+        for(int i = 0; i < 26; i++)
+        if(count[i] > 0)
+        {
+            printf("%c ", i + 'a');
+            printf("%d\n", count[i]);
+        }
+    }
+    return 0;
+}
