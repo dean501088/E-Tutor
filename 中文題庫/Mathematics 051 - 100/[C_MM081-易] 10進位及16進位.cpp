a92b0509c145e0d4f267701dba24497a97466ed8
noname <noname19930930@gmail.com>
2016-04-28 12:33:00 +0800

    AC

---
+++ 中文題庫/Mathematics 051 - 100/[C_MM081-易] 10進位及16進位.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string s;
+     
+    while(cin >> s)
+    {
+        int temp;
+        if(s[0] == '0' && s[1] == 'x')
+        {
+            istringstream(s) >> hex >> temp;
+            cout << dec << temp << endl;  
+        }
+        else
+        {
+            istringstream(s) >> dec >> temp;
+            cout << "0x";
+            cout << hex << uppercase << temp << endl;
+        }
+    }
+    return 0;
+}
