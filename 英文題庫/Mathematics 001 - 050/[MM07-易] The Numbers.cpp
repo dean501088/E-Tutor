21bf7a674bffd74259b44495bbbb6fa5e051bdea
noname <noname19930930@gmail.com>
2016-05-01 00:18:20 +0800

    AC

---
+++ 英文題庫/Mathematics 001 - 050/[MM07-易] The Numbers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string n, m;
+    while(cin >> n >> m)
+    {
+        int counter = 0;
+        for(int i = 0; i < m.length(); i++)
+        {
+            bool ok = true;
+            for(int j = 0; j < n.length() && ok; j++)
+                if(m[i + j] != n[j])
+                    ok = false;
+            if(ok)
+                counter++;
+        }
+        cout << counter << endl;
+    }
+    return 0;
+}
