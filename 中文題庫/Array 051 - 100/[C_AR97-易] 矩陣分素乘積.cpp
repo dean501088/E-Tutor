cc34adb9a5d82effc2a67e65a7d961dcc94b4cac
noname <noname19930930@gmail.com>
2016-05-04 03:06:46 +0800

    AC

---
+++ 中文題庫/Array 051 - 100/[C_AR97-易] 矩陣分素乘積.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+  
+using namespace std;
+  
+int main()
+{
+    int n, m;
+    while(cin >> n >> m)
+    {
+        //input
+        int x[20][20];
+        for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> x[i][j];
+          
+        int y[20][20];
+        for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> y[i][j];
+          
+        //product
+        for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            x[i][j] *= y[i][j];
+          
+        //output
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                cout << x[i][j];
+                if(j < m - 1)
+                    cout << " ";
+            }
+            cout << endl;
+        }   
+    }
+    return 0;
+}
