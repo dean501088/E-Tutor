2d376499632d38925f1d66023ef3b1d732b2ccd0
noname <noname19930930@gmail.com>
2016-05-08 05:26:11 +0800

    AC

---
+++ ITSA/036/36 - [Problem 3] 分區停水.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	int k;
+	while (cin >> k)
+	{
+		while (k--)
+		{
+			//input
+			int n; cin >> n;
+
+			for (int m = n - 1; m > -1; m--)
+			{
+				//initilize
+				vector<int> religion;
+				for (int i = 0; i < n; i++)
+					religion.push_back(i + 1);
+
+				int index = 0;
+
+				while (religion.size() > 1)
+				{
+					index += m - 1;
+					index %= religion.size();
+					religion.erase(religion.begin() + index);
+				}
+				if (religion[0] == n)
+				{
+					cout << m << endl;
+					break;
+				}
+			}
+		}
+	}
+}
