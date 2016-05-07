e151fde707e4a0d557753970e5d48837e03fd662
noname <noname19930930@gmail.com>
2016-05-07 20:19:54 +0800

    AC

---
+++ 中文題庫/Mathematics 151 - 200/[C_MM163-易] 輾轉相除法.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int gcd(int x, int y)
+{
+    while (x %= y)
+        swap(x, y);
+    return y;
+}
+ 
+int main()
+{
+    int n, m;
+    while(cin >> n >> m)
+    printf("%d\n", gcd(n, m));
+    return 0;
+}
