476e10bf70bcf0a20488e60be8b69e02d2e76a22
noname <noname19930930@gmail.com>
2016-05-19 00:45:17 +0800

    AC

---
+++ 中文題庫/Other 001 - 050/[C_OT08-易] 最佳排程.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+  
+int main()
+{
+    int n; cin >> n;
+    while (n--)
+    {
+        vector<int> time;
+  
+        int max; cin >> max;
+        while (true)
+        {
+            int temp; cin >> temp;
+              
+            if (temp == 0)
+                break;
+            else
+            {
+                time.push_back(temp);
+            }
+        }
+        sort(time.begin(), time.end());
+  
+        int count = 0;
+        for (int i = 0; i < time.size(); i++)
+        {
+            max -= time[i];
+  
+            if (max < 0)
+                break;
+  
+            count++;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
