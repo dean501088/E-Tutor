d124f42eadcc324daea3f52879960d2c76340002
noname <noname19930930@gmail.com>
2016-06-05 06:50:20 +0800

    AC

---
+++ 中文題庫/Mathematics 001 - 050/[C_MM18-易] 十進制轉二進制.cpp
@@ -0,0 +1,26 @@
+#include<iostream>  
+using namespace std;  
+int main()  
+{  
+    int x;  
+    while(cin >> x)  
+    {  
+        int y[8];  
+        if(x>0)  
+            y[0]=0;  
+        else 
+        {  
+            y[0]=1;  
+            x+=128;  
+        }  
+        for(int i=0;i<7;i++)  
+        {  
+            y[7-i]=x%2;  
+            x/=2;  
+        }  
+        for(int i=0;i<8;i++)  
+            cout << y[i];  
+        cout << endl;  
+    }  
+    return 0;  
+}  
