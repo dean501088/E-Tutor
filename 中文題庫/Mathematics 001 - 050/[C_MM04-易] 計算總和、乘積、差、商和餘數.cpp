b2961b62529d30166417d7f1c569b835add4574d
noname <noname19930930@gmail.com>
2016-06-26 20:53:10 +0800

    AC

---
+++ 中文題庫/Mathematics 001 - 050/[C_MM04-易] 計算總和、乘積、差、商和餘數.cpp
@@ -0,0 +1,20 @@
+#include<iostream>  
+
+using namespace std; 
+ 
+int main()  
+{  
+    int x, y;  
+    while(cin >> x >> y)  
+    {  
+        cout << x << "+" << y << "=" << x+y << endl;  
+        cout << x << "*" << y << "=" << x*y << endl;  
+        cout << x << "-" << y << "=" << x-y << endl;  
+        cout << x << "/" << y << "=";  
+        if(x<0)  
+            cout << x/y-1 << "..." << x%y+y << endl;  
+        else  
+            cout << x/y << "..." << x%y << endl;  
+    }  
+    return 0;  
+}  
