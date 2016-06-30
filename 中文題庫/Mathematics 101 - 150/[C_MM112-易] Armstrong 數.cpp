5598f2fd85225307445badffe75d36555bc2a898
noname <noname19930930@gmail.com>
2016-07-01 01:11:06 +0800

    AC

---
+++ 中文題庫/Mathematics 101 - 150/[C_MM112-易] Armstrong 數.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int armstrong[4] = { 153, 370, 371, 407 };
+    for (int i = 0; i < 4; i++)
+        cout << armstrong[i] << endl;
+    return 0;
+}
