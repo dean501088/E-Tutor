be96f781ca248f1ede4ed63e3f7054dd486dc93a
noname0930 <noname19930930@gmail.com>
2017-03-27 01:30:54 +0800

    cp from solution

---
+++ ITSA/044/44 - [Problem 1] 判斷座標是否在正方形的範圍內.cpp
@@ -0,0 +1,14 @@
+#include<iostream>  
+using namespace std;  
+int main()  
+{  
+    int x, y;  
+    while(cin >> x >> y)  
+    {  
+        if(x<=100 && y<=100)  
+            cout << "inside" << endl;  
+        else 
+            cout <<"outside" << endl;  
+    }  
+    return 0;  
+}  
